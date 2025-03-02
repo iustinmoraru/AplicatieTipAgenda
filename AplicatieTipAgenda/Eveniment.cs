6a88e12a588ef962cde9be48a78b0f06c3a2bc33
Iustin <Iustin@DESKTOP-P0HIFTG>
2025-03-02 11:07:10 +0200

    Add project files.

---
+++ AplicatieTipAgenda/Eveniment.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicatieTipAgenda
+{
+    class Eveniment
+    {
+        string Titlu, Descriere;
+        DateTime Data;
+
+        // Constructor
+        public Eveniment(string _Titlu, DateTime? _Data = null, string _Descriere = "")
+        {
+            Titlu = _Titlu;
+            Data = _Data ?? DateTime.Now;  // Dacă _Data este null, setează la data curentă (DateTime.Now)
+            Descriere = _Descriere;
+        }
+
+        public override string ToString()
+        {
+            return $"Eveniment: {Titlu}\nData: {Data.ToString("dd/MM/yyyy HH:mm")}\nDescriere: {Descriere}";
+        }
+
+        public string GetTitlu()
+        {
+            return Titlu;
+        }
+        public DateTime GetData()
+        {
+            return Data;
+        }
+        public string GetDescriere()
+        {
+            return Descriere;
+        }
+        public void SetTitlu(string TitluNou)
+        {
+            Titlu = TitluNou;
+        }
+        public void SetData(DateTime DataNoua)
+        {
+            Data = DataNoua;
+        }
+        public void SetDescriere(string DescriereNoua)
+        {
+            Descriere = DescriereNoua;
+        }
+    }
+}
---
+++ AplicatieTipAgenda/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicatieTipAgenda
+{
+    class Program
+    {
+        static void Main()
+        {
+            Eveniment ev1 = new Eveniment("as", new DateTime(2025, 2, 1), "asdasd");
+            Console.WriteLine(ev1.ToString());
+
+            Agenda agenda = new Agenda();
+        }
+    }
+}
